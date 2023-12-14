00cc14acbf7b6fe6813c9287b32085f974bebb86
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-12-14 12:36:34 +0000

    fix: increase health by spice

--- my-app/src/components/Zoo.tsx
+++ my-app/src/components/Zoo.tsx
@@ -7,7 +7,7 @@ import './../styles.css';
 
 const Zoo: React.FC = () => {
   const [animals, SetAnimals] = useState<IAnimal[]>([]);
-  const [ intervalId, SetIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [intervalId, SetIntervalId] = useState<NodeJS.Timeout | null>(null);
 
   const location = useLocation();
   const newZoo: INewZoo = location.state?.newZoo;
@@ -21,63 +21,96 @@ const Zoo: React.FC = () => {
   }, []);
 
   const startInterval = () => {
-  const intervalId =  setInterval (() => {
-    handledTime();
-  }, 3600000)
-  SetIntervalId(intervalId);
-}
-
-
-    const handledTime = () => {
-      if(intervalId){
-        clearInterval(intervalId);
-        SetAnimals(decreaseHealth(animals));
-        startInterval();
-      }
-      SetAnimals(decreaseHealth(animals));
-    }
-    const handledFood = () => {
-      SetAnimals(increaseHealth(animals));
-    }
-    const handledNewZoo = () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-      navigate('/')
-    }
-
-    return (
-      <section id="zoo">
-        <h1 className='zoo-welcome'>Welcome to {newZoo.zooName}!</h1>
-        <div className='zoo-buttons'>
-          <img
-            onClick={handledTime}
-            className='zoo-buttons-icon'
-            src='./timeIcon.png'
-            alt='Time Icon' />
-          <img
-            onClick={handledFood}
-            className='zoo-buttons-icon'
-            src='./foodIcon.png'
-            alt='Food Icon' />
-        </div>
-
-        <div className='zoo-animals-grid'>
-          <div className='zoo-animals-grid-cage cage-a'>
-            <Animals animals={newZoo.animals.filter((animal) => animal.type === 'giraffe')} />
-          </div>
-          <div className='zoo-animals-grid-cage cage-b'>
-            <Animals animals={newZoo.animals.filter((animal) => animal.type === 'elephant')} />
-          </div>
-          <div className='zoo-animals-grid-cage cage-c'>
-            <Animals animals={newZoo.animals.filter((animal) => animal.type === 'monkey')} />
-          </div>
-        </div>
-
-        <button className='newZoo-button' type='submit' onClick={handledNewZoo}>New Zoo</button>
-
-      </section>
-    );
+    const intervalId = setInterval(() => {
+      handledTime();
+    }, 3600000)
+    SetIntervalId(intervalId);
   }
 
-  export default Zoo;
+
+  const handledTime = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      SetAnimals(decreaseHealth(animals));
+      startInterval();
+    }
+    SetAnimals(decreaseHealth(animals));
+  };
+
+  const handledFood = () => {
+    const increaseGiraffeHealth = increaseHealth();
+    const increaseMonkeyHealth = increaseHealth();
+    const increaseElephantHealth = increaseHealth();
+
+    const updateAnimalsHealthFeeding = animals.map((animal) => {
+      if (animal.type === 'giraffe' && animal.health >= 50) {
+        const newHealth = animal.health + increaseGiraffeHealth;
+        if (newHealth >= 100) {
+          animal.health = 100;
+        } else {
+          animal.health = newHealth;
+        }
+      }
+      else if (animal.type === 'monkey' && animal.health >= 30) {
+        const newHealth = animal.health + increaseMonkeyHealth;
+        if (newHealth >= 100) {
+          animal.health = 100;
+        } else {
+          animal.health = newHealth;
+        }
+      }
+      else if (animal.type === 'elephant' && animal.health >= 70) {
+        const newHealth = animal.health + increaseElephantHealth;
+        if (newHealth >= 100) {
+          animal.health = 100;
+        } else {
+          animal.health = newHealth;
+        }
+      }
+      return animal
+    })
+    SetAnimals(updateAnimalsHealthFeeding)
+  };
+
+  const handledNewZoo = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    navigate('/')
+  }
+
+  return (
+    <section id="zoo">
+      <h1 className='zoo-welcome'>Welcome to {newZoo.zooName}!</h1>
+      <div className='zoo-buttons'>
+        <img
+          onClick={handledTime}
+          className='zoo-buttons-icon'
+          src='./timeIcon.png'
+          alt='Time Icon' />
+        <img
+          onClick={handledFood}
+          className='zoo-buttons-icon'
+          src='./foodIcon.png'
+          alt='Food Icon' />
+      </div>
+
+      <div className='zoo-animals-grid'>
+        <div className='zoo-animals-grid-cage cage-a'>
+          <Animals animals={newZoo.animals.filter((animal) => animal.type === 'giraffe')} />
+        </div>
+        <div className='zoo-animals-grid-cage cage-b'>
+          <Animals animals={newZoo.animals.filter((animal) => animal.type === 'elephant')} />
+        </div>
+        <div className='zoo-animals-grid-cage cage-c'>
+          <Animals animals={newZoo.animals.filter((animal) => animal.type === 'monkey')} />
+        </div>
+      </div>
+
+      <button className='newZoo-button' type='submit' onClick={handledNewZoo}>New Zoo</button>
+
+    </section>
+  );
+}
+
+export default Zoo;
--- my-app/src/functions.tsx
+++ my-app/src/functions.tsx
@@ -1,32 +1,19 @@
-import {IAnimal} from './interfaces';
+import { IAnimal } from './interfaces';
 
 export const decreaseHealth = (animals: IAnimal[]) => {
     const updateAnimalsHealth = animals.map((animal: IAnimal) => {
-        const animalHealth = animal.health
-        if (animalHealth >= 30) {
-            const decrease = Math.floor(Math.random() * 21);
-            animal.health = Number(animalHealth) - decrease;
+        if (animal.health >= 30) {
+        const animalHealth = animal.health;
+        const decrease = Math.floor(Math.random() * 21);
+        animal.health = Number(animalHealth) - decrease;
         }
         return animal;
-
     })
-   return updateAnimalsHealth;
+    return updateAnimalsHealth;
 }
 
 
-export  const increaseHealth = (animals: IAnimal[]) => {
+export const increaseHealth = () => {
     const increase = Math.floor(Math.random() * 16) + 10;
-
-    const updateAnimalsHealth = animals.map((animal) => {
-        const animalHealth = animal.health
-        if (animalHealth >= 30) {
-            animal.health = Number(animalHealth) + increase;
-            if (animal.health >= 100) {
-                animal.health = 100;
-            }
-        }
-        return animal;
-
-    })
-    return updateAnimalsHealth;
+    return increase;
 }
